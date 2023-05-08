6094993a5246cbbc1fdd731f7a61a7ed6d8a38e3
Tobias Augustyniak <tobias.augustyniak@t-online.de>
2023-05-08 16:00:09 +0200

    Task 2 completed, add component spotlight

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,37 @@
 import { GlobalStyle } from "../styles";
 import useSWR from "swr";
 import ArtPieces from "../components/artpieces";
+import React from "react";
 
-const fetcher = (url) => fetch(url).then((res) => res.json());
+function pickRandomPiece(pieces) {
+  const randomIndex = Math.floor(Math.random() * pieces.length);
+  return pieces[randomIndex];
+}
+
+function Spotlight({ image, artist }) {
+  return (
+    <div>
+      <h2>Spotlight Piece</h2>
+      <img src={image} alt={artist} />
+      <p>Artist: {artist}</p>
+    </div>
+  );
+}
 
 export default function HomePage() {
-  const { data, error } = useSWR("/api/artPieces", fetcher);
+  const URL = "https://example-apis.vercel.app/api/art";
+  const { data: pieces } = useSWR(URL);
 
-  if (error) return <div>Failed to load</div>;
-  if (!data) return <div>Loading...</div>;
+  if (!pieces) {
+    return <p>Loading...</p>;
+  }
 
-  return <ArtPieces pieces={data} />;
+  const randomPiece = pickRandomPiece(pieces);
+
+  return (
+    <div>
+      <Spotlight image={randomPiece.image} artist={randomPiece.artist} />
+      <ArtPieces pieces={pieces} />
+    </div>
+  );
 }
